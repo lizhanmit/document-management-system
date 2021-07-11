64abdd35d744cd00d41cc81b057995ed21f43003
lizhanmit <lizhan_mit@outlook.com>
2021-07-11 20:23:00 +0800

    Added importFile().

--- src/main/java/DocumentManagementSystem.java
+++ src/main/java/DocumentManagementSystem.java
@@ -1,10 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DocumentManagementSystem {
 
-    private Map<String, Importer> extensionToImporter = new HashMap<>();
+    private final List<Document> documents = new ArrayList<>();
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
 
     public DocumentManagementSystem() {
         extensionToImporter.put("letter", new LetterImporter());
@@ -12,8 +17,31 @@ public class DocumentManagementSystem {
         extensionToImporter.put("jpg", new ImageImporter());
     }
 
-    public void importFile(String path) {
+    public void importFile(String path) throws IOException {
+        final File file = new File(path);
 
+        if (!file.exists()) {
+            throw new FileNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf(".");
+
+        if (separatorIndex != -1) {
+            if (separatorIndex == path.length() - 1) {
+                throw new UnknownFileTypeException("No extension found For file: " + path);
+            }
+
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+            if (importer == null) {
+                throw new UnknownFileTypeException("No importer found for file: " + path);
+            }
+
+            final Document document = importer.importFile(file);
+            documents.add(document);
+        } else {
+            throw new UnknownFileTypeException("No extension found For file: " + path);
+        }
     }
 
     public List<Document> contents() {
